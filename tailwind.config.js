42ecf8dfa754d82c38e304e81be01e097c8bc723
sonalan <sinan@onalan.net>
2025-03-30 22:18:15 -0700

    fix app path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 }

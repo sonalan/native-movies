7619282de74e83d1f689c89fcce63de8fb8a04f9
sonalan <sinan@onalan.net>
2025-04-01 00:00:17 -0700

    adds tabs structer

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center background-dark"
+    >
+      <Text className="text-5xl text-accent font-bold">Hellloooo!</Text>
+      
+    </View>
+  );
+}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View
+        className="flex-1 justify-center items-center background-dark"
+    >
+      <Text className="text-5xl text-accent font-bold">Profile</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})

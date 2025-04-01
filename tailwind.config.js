76747ee9a17207fba8a68cfaa8652e3f4e09b14d
sonalan <sinan@onalan.net>
2025-04-01 00:08:50 -0700

    fixes text styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
       colors: {
         primary: "#030014",
         secondary: "#151312",
-        "light": {
-          "100": "#D6C6FF",
-          "200": "#A8B5DB",
-          "300": "#9CA4AB"
+        light: {
+          100: "#D6C7FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
         },
-        "dark": {
-          "100": "#221f3d",
-          "200": "#0f0d23"
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
         },
         accent: "#AB8BFF",
       },

c6cc7961c37864c6a29316ed0f1b7367775544eb
sonalan <sinan@onalan.net>
2025-04-01 20:04:21 -0700

    updates home screen styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Text, View, Image, ScrollView } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 justify-center items-center background-dark"
-    >
-      <Text className="text-5xl text-accent font-bold">Hellloooo!</Text>
-      
+    <View className="flex-1 bg-primary" >
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }

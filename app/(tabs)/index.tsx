4579835fc8ec3c5f24b094c3245429b06bd711aa
sonalan <sinan@onalan.net>
2025-04-03 20:57:32 -0700

    adds popular movies to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { View, Image, ScrollView } from "react-native";
+import { View, Image, Text, ScrollView, ActivityIndicator, FlatList } from "react-native";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/tmdb.api";
 
 export default function Index() {
   const router = useRouter();
+
+  const { 
+    data : movies,
+    loading: moviesLoading, 
+    error: moviesError,  
+
+  } = useFetch(() => fetchMovies({
+    keyword: ''
+  }));
+
   return (
     <View className="flex-1 bg-primary" >
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -20,6 +32,35 @@ export default function Index() {
             onPress={() => router.push("/search")}
             placeholder="Search for a movie"
           />
+          {moviesLoading ? (
+            <ActivityIndicator 
+              size="large"
+              color="#fff"
+            />
+          ): moviesError ? (
+            <Text className="text-white">{moviesError?.message}</Text>
+          ) : (
+            <>
+              <Text className="text-white text-lg font-bold mt-10 border-b-orange-700 border-b-4">Popular Movies</Text>
+              <FlatList
+                data={movies}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                className="mt-5 pd-32"
+                columnWrapperStyle={{ 
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10
+
+                }}
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm">{item.title}</Text>
+                )}
+              />
+            </>
+          ) 
+          }
         </View>
       </ScrollView>
     </View>

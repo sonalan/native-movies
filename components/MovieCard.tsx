9ce456b10b56c03f8d4a8861c4afcf8670dfa7ca
sonalan <sinan@onalan.net>
2025-04-03 23:00:40 -0700

    fix poster image issue

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -10,15 +10,16 @@ const MovieCard = ({
     vote_average,
     overview,
 }: Movie) => {
+    //console.log(poster_path)
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
         <TouchableOpacity className='w-[30%]'>
             <Image
                 source={{ uri: poster_path 
                     ? `https://image.tmdb.org/t/p/w500/${poster_path}` 
                     : 'https://palceholder.com/600x400/1a1a1a/ffffff.png'
                 }}
-                className='w-full h-53 rounded-lg'
+                className='w-full h-52 rounded-lg'
                 resizeMode='cover'
             />
             <Text className='text-white text-sm text-bold mt-2'>{title}</Text>

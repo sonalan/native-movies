726b35d5be6263c8c58c4ddac8b66a2897b2d589
sonalan <sinan@onalan.net>
2025-04-04 23:42:15 -0700

    adds fetchMovie implementation

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { useRoute } from '@react-navigation/native'
+import useFetch from '@/services/useFetch'
+import { fetchMovie } from '@/services/tmdb.api'
+
 
 const MovieDetails = () => {
+  const route = useRoute()
+  const { id } = route.params;
+  const { 
+    data: movie ,
+    loading, 
+    error,  
+
+  } = useFetch(() => fetchMovie({
+    id: id
+  }));
   return (
     <View>
-      <Text>[id]</Text>
+      {loading
+        ? <Text>Loading...</Text>
+        : error? <Text>Error</Text>
+        : <Text>{movie?.title}</Text>
+      }
     </View>
   )
 }
 
 export default MovieDetails
-
-const styles = StyleSheet.create({})
--- services/tmdb.api.ts
+++ services/tmdb.api.ts
@@ -1,12 +1,15 @@
+//const api_key = process.env.TMDB_API_PUBLIC_KEY;
+const api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDYwNDVmNWFmZjhmOGUwODA0M2E0MmY4MWFjNDBlZSIsIm5iZiI6MTUyODQ4NjU0NC4xNjgsInN1YiI6IjViMWFkYTkwMGUwYTI2MWY5ZDAxNjY5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IZRy6gsUrfmrj0kkNqaNDuayOJn29ysJitkY_P2NOXc';
 export const TMDB_CONFIG = {
-    API_KEY: process.env.TMDB_API_PUBLIC_KEY,
+    API_KEY: api_key,
     API_URL: 'https://api.themoviedb.org/3/',
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_API_PUBLIC_KEY}`,
+        Authorization: `Bearer ${api_key}`,
     },
 };
 
+//console.log('key', TMDB_CONFIG.API_KEY)
 export const fetchMovies = async ({keyword}: {keyword: string}) => {
     //keyword/{keyword_id}/movies
     //movie/popular?language=en-US&page=1
@@ -25,4 +28,20 @@ export const fetchMovies = async ({keyword}: {keyword: string}) => {
     }
     const data = await response.json();
     return data.results;
+}
+
+export const fetchMovie = async ({id}: {id: string}) => {
+    const endpoint = `movie/${id}?language=en-US`;
+    const response = await fetch(`${TMDB_CONFIG.API_URL}${endpoint}`, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+        // @ts-ignore
+        throw new Error('Failed to fetch movie', response.statusText);
+    }
+    const data = await response.json();
+    console.log('data', data)
+    return data;
 }

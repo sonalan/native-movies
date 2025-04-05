ed2d56d7fc84274c5a1a5d040c4059b8635651fb
sonalan <sinan@onalan.net>
2025-04-04 23:43:40 -0700

    adds searchform to search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,16 +1,99 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, FlatList, Image, Text, View,  NativeSyntheticEvent, TextInputFocusEventData } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { images } from '@/constants/images'
+import MovieCard from '@/components/MovieCard'
+import useFetch from '@/services/useFetch'
+import { fetchMovies } from '@/services/tmdb.api'
+import { icons } from '@/constants/icons'
+import { SearchBar } from 'react-native-screens'
 
 const Search = () => {
+  // const route = useRoute();
+  // const keyWord = route.params?.keyword;
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const { 
+    data : movies,
+    loading, 
+    error,  
+    refetch: loadMovies,
+    reset
+
+  } = useFetch(() => fetchMovies({
+    keyword: searchKeyword,
+  }), false);
+
+  const handleSearch = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    const text = (e as any).nativeEvent.text || '';
+    setSearchKeyword(text);
+  }
+  
+  useEffect(() => {
+    const func = async () => {
+      if(searchKeyword.trim()){
+        await loadMovies();
+      }else{
+         reset();
+      }
+    }
+    func();
+  }, [searchKeyword])
+
   return (
     <View
-        className="flex-1 justify-center items-center background-dark"
+        className="flex-1 bg-primary"
     >
-      <Text className="text-5xl text-accent font-bold">Search</Text>
+      <Image 
+        source={images.bg} 
+        className="absolute w-full z-0" 
+        resizeMode="cover"
+      
+      />
+      <FlatList 
+        data={movies}
+        numColumns={3}
+        className="mt-5 pd-32"
+        keyExtractor={(item) => item.id.toString()}
+        columnWrapperStyle={{ 
+          justifyContent: "flex-start",
+          gap: 20,
+          paddingRight: 5,
+          marginBottom: 10
+
+        }}
+        renderItem={({item}) => <MovieCard {... item} />}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchKeyword}
+                onChangeText={handleSearch}     
+              />
+            </View>
+            
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+          </>
+        }
+      
+      />
+
     </View>
   )
 }
 
 export default Search
-
-const styles = StyleSheet.create({})
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,10 +4,12 @@ import { icons } from '@/constants/icons'
 
 interface Props{
     placeholder: string,
-    onPress?: () => void
+    onPress?: () => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
         <Image 
@@ -18,9 +20,9 @@ const SearchBar = ({placeholder, onPress}: Props) => {
         />
         <TextInput 
             onPress={onPress}
-            onChangeText={() => {}}
+            onChangeText={onChangeText}
             placeholder={placeholder}
-            value=''
+            value={value}
             placeholderTextColor='#a8b5db' 
             className='flex-1 ml-2 text-white' 
         />

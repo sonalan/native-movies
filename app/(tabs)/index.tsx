48885435e510ba71af58b2b29e4fde589764df59
sonalan <sinan@onalan.net>
2025-04-05 16:55:40 -0700

    fixes VirtualizedLists should never be nested issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { View, Image, Text, ScrollView, ActivityIndicator, FlatList } from "react-native";
+import { View, Image, Text, ActivityIndicator, FlatList } from "react-native";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
@@ -11,61 +11,60 @@ export default function Index() {
   const router = useRouter();
 
   const { 
-    data : movies,
+    data: movies,
     loading: moviesLoading, 
     error: moviesError,  
+  } = useFetch(() => fetchMovies({ keyword: '' }));
 
-  } = useFetch(() => fetchMovies({
-    keyword: ''
-  }));
+  if (moviesLoading) {
+    return (
+      <View className="flex-1 bg-primary justify-center items-center">
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    );
+  }
+
+  if (moviesError) {
+    return (
+      <View className="flex-1 bg-primary justify-center items-center">
+        <Text className="text-white">{moviesError?.message}</Text>
+      </View>
+    );
+  }
+
+  const ListHeader = () => (
+    <>
+      <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      <SearchBar
+        onPress={() => {
+          router.push("/search");
+        }}
+        placeholder="Search for a movie"
+      />
+      <Text className="text-white text-lg font-bold mt-10 border-b-orange-700 border-b-4">
+        Popular Movies
+      </Text>
+    </>
+  );
 
   return (
-    <View className="flex-1 bg-primary" >
+    <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
-      <ScrollView
-        className="flex-1 px-5"
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
-      >
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-              onPress={() => {
-                router.push("/search");
-              }}
-              placeholder="Search for a movie"
-            />
-          {moviesLoading ? (
-            <ActivityIndicator 
-              size="large"
-              color="#fff"
-            />
-          ): moviesError ? (
-            <Text className="text-white">{moviesError?.message}</Text>
-          ) : (
-            <>
-              <Text className="text-white text-lg font-bold mt-10 border-b-orange-700 border-b-4">Popular Movies</Text>
-              <FlatList
-                data={movies}
-                keyExtractor={(item) => item.id.toString()}
-                numColumns={3}
-                className="mt-5 pd-32"
-                columnWrapperStyle={{ 
-                  justifyContent: "flex-start",
-                  gap: 20,
-                  paddingRight: 5,
-                  marginBottom: 10
-
-                }}
-                renderItem={({ item }) => (
-                  <MovieCard {... item} />
-                )}
-              />
-            </>
-          ) 
-          }
-        </View>
-      </ScrollView>
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="px-5"
+        columnWrapperStyle={{ 
+          justifyContent: "flex-start",
+          gap: 20,
+          paddingRight: 5,
+          marginBottom: 10
+        }}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={{ paddingBottom: 10 }}
+        renderItem={({ item }) => <MovieCard {...item} />}
+      />
     </View>
   );
 }
